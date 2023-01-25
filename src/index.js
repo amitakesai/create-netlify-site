ed14fd7f7fb8e5049a3e6b2524a9ae7f816f141b
amitakesai <amita.kesai@unitedtalent.com>
2023-01-25 16:27:56 +0530

    add comments

--- src/index.js
+++ src/index.js
@@ -8,6 +8,14 @@ const netlifyTeam = core.getInput('netlify-team');
 const branchName = core.getInput('branch-name');
 const api = new NetlifyAPI(netlifyToken);
 
+if (!targetAppName || !repoName || !netlifyToken || !netlifyTeam || !branchName) {
+    throw new Error("Arg 'unique-site-name' or 'repo-name' or 'netlify-token' or 'branch-name' must not be empty") ;
+}
+
+if(!api) {
+    throw new Error("Error while getting NetlifyAPI, please check 'netlify-token'");
+}
+
 const site = async () => {
     const result = await api.createSite({
         account_slug: netlifyTeam,
@@ -21,6 +29,10 @@ const site = async () => {
             },
         } 
     });
+    if(!result) {
+        throw new Error("Error while getting NetlifyAPI, please check 'netlify-token'");
+    }
+    core.info(`Netlify site URL: ${result.url} and site-id: ${result.site_id}`)
     core.setOutput('site-id', result.site_id)
     core.setOutput('site-url', result.url)
 }
